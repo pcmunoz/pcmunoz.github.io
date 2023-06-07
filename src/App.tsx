6645346461e600e7dc21349c54df564fa6195c86
Patrick Ceasar Munoz <munozpatrickceasar@gmail.com>
2023-06-07 09:35:56 +0800

    Fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react"
 // import './App.css';
 import Header from "./Header"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Header />
